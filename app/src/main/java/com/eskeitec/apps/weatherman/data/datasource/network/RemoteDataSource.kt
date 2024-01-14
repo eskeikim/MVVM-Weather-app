4fd54215cf2d96f561947d86aeb9cc72f19789be
eskeikim <eskeiblue90@gmail.com>
2024-01-14 08:43:30 +0300

    Add secrete keys to gradle

--- app/src/main/java/com/eskeitec/apps/weatherman/data/datasource/network/RemoteDataSource.kt
+++ app/src/main/java/com/eskeitec/apps/weatherman/data/datasource/network/RemoteDataSource.kt
@@ -1,16 +1,16 @@
 package com.eskeitec.apps.weatherman.data.datasource.network
 
-import com.eskeitec.apps.weatherman.utils.Constants
+import com.eskeitec.apps.weatherman.BuildConfig
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(private val apiService: WeatherApiService) {
     suspend fun getCurrentWeather(
         lat: String,
         lon: String,
-    ) = apiService.getCurrentWeather(lat, lon, Constants.API_KEY)
+    ) = apiService.getCurrentWeather(lat, lon, BuildConfig.OPEN_API_KEY)
 
     suspend fun getDayForecast(
         lat: String,
         lon: String,
-    ) = apiService.getDaysForecast(lat, lon, Constants.API_KEY)
+    ) = apiService.getDaysForecast(lat, lon, BuildConfig.OPEN_API_KEY)
 }
